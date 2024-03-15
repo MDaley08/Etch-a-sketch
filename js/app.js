f6ae808ec128c278b64bef91147cc7fa51629905
Michael Daley <mdaley08@outlook.com>
2024-03-15 02:52:21 -0400

    implemented button and function to clear drawing on canvas

--- js/app.js
+++ js/app.js
@@ -1,6 +1,19 @@
 const canvas  = document.getElementById('canvas');
+const gridInput = document.getElementById('grid-size');
+const gridBtn = document.getElementById('grid-btn')
+const clearBtn = document.getElementById('clear');
 const boxArr = [];
+const filledBoxArr = [];
 
+const editGridSize = () => {
+    let inputVal = gridInput.value;
+    gridInput.value = '';
+}
+const clearGrid = () => {
+    clearBtn.addEventListener('click', () => {
+        filledBoxArr.forEach(element => element.style.backgroundColor = 'white')
+    });
+};
 const createGrid = (gridSize) => {
     const boxLen = (canvas.offsetHeight/gridSize) + 'px';
     for(let i = 0; i < gridSize; i++){
@@ -11,14 +24,19 @@ const createGrid = (gridSize) => {
                                  height:${boxLen}; 
                                  width:${boxLen}; 
                                  box-sizing: border-box;
-                                 background-color: white;`
+                                 background-color: white;`;
             boxArr.push(box);
             column.appendChild(box);
         }
         canvas.appendChild(column);
     }
     boxArr.forEach(element => {
-        element.addEventListener('mouseover',() => alert('temp') )
+        element.addEventListener('mouseover',() => {
+            filledBoxArr.push(element);
+            element.style.backgroundColor = 'black'
+        });
     });
-}
+};
+clearGrid();
+editGridSize();
 createGrid(100);
